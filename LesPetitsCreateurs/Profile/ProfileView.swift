27a1e8474bbb0f2353517ecb9585e0f7d2fcabd7
Valentin Vanhove <92478691+vanhovev@users.noreply.github.com>
2024-11-06 19:24:51 +0100

    feat: Sheet on profle

--- LesPetitsCreateurs/Profile/ProfileView.swift
+++ LesPetitsCreateurs/Profile/ProfileView.swift
@@ -9,13 +9,37 @@ import SwiftUI
 
 struct ProfileView: View {
     @State private var presentedProfiles: [ProfileModel] = []
+    @State private var showingSheet: Bool = false
     
     var body: some View {
         NavigationStack {
             VStack {
-               
+                
+                Image("user-pict")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 100.0, height: 100.0)
+                    .clipShape(Circle())
+                    
+                
+                Text("John Appleseed")
+                    .font(.title)
+                    .fontWeight(.bold)
+                
+                Spacer()
+                
             }
             .navigationTitle("Profile")
+
+            .onTapGesture {
+                showingSheet.toggle()
+            }
+            .sheet(isPresented: $showingSheet) {
+                VStack {
+                    Text("Hello, World!")
+                        .font(.headline)
+                }
+            }
         }
         .background(Color.white)
     }
